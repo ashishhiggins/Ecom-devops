5dffa6b68e98053cfd136f8e6b870742bc6f052b
ASHISH HIGGINS <higginsashish@icloud.com>
2025-12-24 19:45:42 +0530

    Terraform fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,27 +3,74 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-deployment"
 }
 
+# --- 1. NETWORKING & REGISTRY ---
 resource "azurerm_container_registry" "acr" {
-  # FIX: Removed quotes so Terraform reads the actual location value
-  location            = azurerm_resource_group.rg.location
-
   name                = "acrdeployment${random_string.suffix.result}"
-
-  # FIX: Removed quotes so Terraform links to the actual Resource Group
   resource_group_name = azurerm_resource_group.rg.name
-
+  location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = true
 }
 
 resource "azurerm_container_app_environment" "env" {
-  # FIX: Removed quotes here as well
-  location            = azurerm_resource_group.rg.location
-
   name                = "env-deployment"
-
-  # FIX: Removed quotes here as well
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
+
+# --- 2. IDENTITY (The "ID Badge") ---
+resource "azurerm_user_assigned_identity" "app_identity" {
+  name                = "ecom-api-identity"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+# --- 3. PERMISSIONS (Give Identity access to ACR) ---
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.app_identity.principal_id
+}
+
+# --- 4. THE APP (Pre-configured with Identity) ---
+resource "azurerm_container_app" "app" {
+  name                         = "ecom-api-v1" # This must match your GitHub Secret
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  # A. Attach the Identity to the App
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.app_identity.id]
+  }
+
+  # B. Tell App to use Identity for this Registry
+  registry {
+    server   = azurerm_container_registry.acr.login_server
+    identity = azurerm_user_assigned_identity.app_identity.id
+  }
+
+  template {
+    container {
+      name   = "java-app"
+      # We use a placeholder image first. GitHub Actions will overwrite this later.
+      image  = "mcr.microsoft.com/k8se/quickstart:latest"
+      cpu    = 0.5
+      memory = "1.0Gi"
+    }
+  }
+
+  # C. Configure Port 8080 (So you don't have to do it manually)
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 8080
+    traffic_weight {
+      percentage = 100
+      latest_revision = true
+    }
+  }
 }
 
 resource "random_string" "suffix" {
@@ -32,7 +79,7 @@ resource "random_string" "suffix" {
   upper   = false
 }
 
-# OUTPUTS
+# --- OUTPUTS ---
 output "acr_name" {
   value = azurerm_container_registry.acr.name
 }
@@ -43,4 +90,12 @@ output "resource_group_name" {
 
 output "container_app_environment_name" {
   value = azurerm_container_app_environment.env.name
+}
+
+output "container_app_name" {
+  value = azurerm_container_app.app.name
+}
+
+output "app_url" {
+  value = azurerm_container_app.app.latest_revision_fqdn
 }
