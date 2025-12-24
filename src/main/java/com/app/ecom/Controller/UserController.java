abf8181f2f7f004c125b7fc02ecc3e86fbba0811
ASHISH HIGGINS <higginsashish@icloud.com>
2025-12-24 19:06:14 +0530

    Changed Java version to 21

--- src/main/java/com/app/ecom/Controller/UserController.java
+++ src/main/java/com/app/ecom/Controller/UserController.java
@@ -2,7 +2,6 @@ package com.app.ecom.Controller;
 
 import com.app.ecom.entity.User;
 import com.app.ecom.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
